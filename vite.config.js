ff47da959b276e65e6192c80255e6638bff5a588
AndriiShvets <a.shvets@devrain.com>
2025-04-25 18:30:03 +0300

    fix file copy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   base: '/article-editor/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...copy({
+        targets: [
+          { src: 'articles/*.md', dest: 'dist/articles' }
+        ],
+        hook: 'writeBundle'
+      }),
+      apply: 'build'
+    }
+  ],
   assetsInclude: ['**/*.md']
 });
