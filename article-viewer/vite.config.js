736046f98f5f04592387b0aead6ca28b486cbc96
AndriiShvets <a.shvets@devrain.com>
2025-04-25 16:43:00 +0300

    changed config

--- article-viewer/vite.config.js
+++ article-viewer/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Заміни 'username' і 'repo-name' на свої
 export default defineConfig({
-  base: '/article-viewer/',
+  base: '/article-editor/',
   plugins: [react()],
   assetsInclude: ['**/*.md']
 });
