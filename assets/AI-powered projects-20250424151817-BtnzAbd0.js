72f1a345aca024f2251365e13f7b7a82ccd6f7c5
shwetsandriy <shwetsandriy@users.noreply.github.com>
2025-05-01 07:43:16 +0000

    deploy: 4bdec41168907430207edee87c79d42b3517692f

--- assets/AI-powered projects-20250424151817-BtnzAbd0.js
+++ assets/AI-powered projects-20250424151817-BtnzAbd0.js
@@ -1,13 +1,15 @@
 const e=`
 # AI-powered projects
 
-**Enabling Quick Access to Corporate Knowledge with Agentic RAG using Azure OpenAI**
+## Enabling Quick Access to Corporate Knowledge with Agentic RAG using Azure OpenAI
 
 [Enabling Quick Access to Corporate Knowledge with Agentic RAG using Azure OpenAI](AI/Enabling-Quick-Access-to-Corporate-Knowledge-with-Agentic-RAG-using-Azure-OpenAI.md)
 
 How the company leveraged Azure services and the innovative Agents and 'ChatGPT on Your Data' approach, incorporating Agentic Retrieval-Augmented Generation (RAG), to manage and utilize a vast collection of documents effectively and handle user queries. This solution significantly streamlined information retrieval, reducing dependency on support specialists and enhancing their efficiency in addressing requests. 
 
-**Intelligent Assistant for Revenue Management platform**
+---
+
+## Intelligent Assistant for Revenue Management platform
 
 [Intelligent Assistant for Revenue Management platform](AI/Intelligent-Assistant-for-Revenue-Management-platform.md)
 
@@ -17,33 +19,45 @@ Copilot allows self-configuration so that users can configure their own KPIs, al
 
 Powered by Azure OpenAI.
 
-**How DevRain’s Legal AI Assistant Helped a US Law Firm**
+---
+
+## How DevRain’s Legal AI Assistant Helped a US Law Firm
 
 [How DevRain’s Legal AI Assistant Helped a US Law Firm](AI/How-DevRain’s-Legal-AI-Assistant-Helped-a-US-Law-Firm.md)
 
 AI-powered Legal assistant for streamlining daily operations in US law firm
 
-**AI-Powered Billing Calculation for Legal Case Documents**
+---
+
+## AI-Powered Billing Calculation for Legal Case Documents
 
 [AI-Powered Billing Calculation for Legal Case Documents](AI/AI-Powered-Billing-Calculation-for-Legal-Case-Documents.md)
 
 Automatic case billing calculation for the case for a US law firm
 
-**Automated processing of legal documents for GDPR compliance**
+---
+
+## Automated processing of legal documents for GDPR compliance
 
 [Automated processing of legal documents for GDPR compliance](AI/Automated-processing-of-legal-documents-for-GDPR-compliance.md)
 
 The semi-automated solution validated documents to see if they met GDPR criteria and prepared a summary of passed and not passed criteria. The person needed to validate the result and approve it.
 
-**Generating PowerShell Scripts with OpenAI**
+---
+
+## Generating PowerShell Scripts with OpenAI
 
 [Generating PowerShell Scripts with OpenAI](AI/Generating-PowerShell-Scripts-with-OpenAI.md)
 
 IT Pro must write custom complex PowerShell scripts to manage companies' infrastructure. OpenAI-powered Copilot generates and validates scripts based on user input. Users can spend 50% less time preparing to use scripts for their needs.
 
-**Kernel: Internal IT Support with Azure OpenAI**
+---
+
+## Kernel: Internal IT Support with Azure OpenAI
 
 [Kernel Internal IT Support with Azure OpenAI](AI/Kernel-Internal-IT-Support-with-Azure-OpenAI.md)
 
 Azure OpenAI-based RAG solution helps employees
+
+---
 `;export{e as default};
